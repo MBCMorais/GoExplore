6de869a4adb4a6c6c2c22cf8c52085800a81340f
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2023-05-22 12:01:47 +0100

    Add project files.

---
+++ GoExplore.Entity/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoExplore.Entity
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public string ReservationDate { get; set; }
+
+        public int Duration { get; set; }
+
+        public Room roomId { get; set; }
+
+        public Customer CustomerId { get; set; }
+
+
+    }
+}
---
+++ GoExplore.Entity/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoExplore.Entity
+{
+    public class Room
+    {
+        public int id { get; set; }
+
+        public string Description { get; set; }
+        public decimal price { get; set; }
+
+        public RoomType type { get; set; }
+
+
+    }
+}
